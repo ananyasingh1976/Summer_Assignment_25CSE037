63cb9a9b21b432935a13e0707687f37e629c4a95
ananyasingh1976 <ananyasingh8966@gmail.com>
2026-06-12 00:37:39 +0530

    Day9 is uploaded

---
+++ Day9/RepeatedCharacter.java
@@ -0,0 +1,21 @@
+//Program to print Reverse Character Pattern 
+
+import java.util.Scanner;
+public class RepeatedCharacter 
+{
+    public static void main(String[] args) 
+    {
+        int rows = 5;
+
+        for (int i = 1; i <= rows; i++) 
+        {
+            char ch = (char) ('A' + i - 1);
+
+            for (int j = 1; j <= i; j++) 
+            {
+                System.out.print(ch);
+            }
+            System.out.println();
+        }
+    }
+}
