7827b064cde75a59777f34b2c4abb74566536dcd
ananyasingh1976 <ananyasingh8966@gmail.com>
2026-06-21 10:08:08 +0530

    Day21 is uploaded

---
+++ Day21/CountWords.java
@@ -0,0 +1,21 @@
+//Program to Count Words in a Sentence
+
+import java.util.Scanner;
+
+public class CountWords {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String str = sc.nextLine().trim();
+
+        if (str.isEmpty()) {
+            System.out.println("Word Count = 0");
+        } else {
+            String[] words = str.split("\\s+");
+            System.out.println("Word Count = " + words.length);
+        }
+
+        sc.close();
+    }
+}
